98feb66e65fe53accdec3f1351b59b5f452e2f58
JK23 <kaczmarek.j@gmail.com>
2020-04-12 23:03:58 +0200

    Draw or Up Draw finished - working .....

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,12 +19,18 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // drawer ON
+    private lateinit var drawerLayout: DrawerLayout
+//    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
@@ -33,16 +39,28 @@ class MainActivity : AppCompatActivity() {
         // integration navController with actionBar !!!!!!!!!!!
         val navController = this.findNavController(R.id.myNavHostFragment)
 
+        //before drawer....
+//        NavigationUI.setupActionBarWithNavController(this, navController)
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        // drawer ON
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
+//        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+//        NavigationUI.setupWithNavController(binding.navView, navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
 //        return super.onSupportNavigateUp()
         val navController = this.findNavController(R.id.myNavHostFragment) //!!!???????
-        return navController.navigateUp()
+        //before drawn
+//        return navController.navigateUp()
 
+
+        //replaces arror back (nav button)  with navigation draw button...
+// -> no such function (on video L3 39 udacity TrivaApp       return NavigationUI.navigateUp(  drawerLayout, navController)
+        return NavigationUI.navigateUp( navController, drawerLayout)
     }
 }
